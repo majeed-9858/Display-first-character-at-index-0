c7f6e24d2e58250483e23f0af04db42375a3fb9f
Majeed Ahmad Hashmi <majeedahmad2727@gmail.com>
2026-02-25 22:59:00 +0500

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Program{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    String str = "Majeed Hashmi";
+    char firstChar = str.charAt(0);
+    System.out.println("The first character is :" + firstChar); 
+    }
+}
